ae3aaed1e894ecf34e10c14b20fdd2a084813a3a
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-04-25 18:10:59 +0300

    Minor updates

--- db/base.py
+++ db/base.py
@@ -94,3 +94,68 @@ class SimpleConcurrentRepository(BaseConcurrentRepository):
             return wrapped
 
         return wrapper
+
+
+class DefaulConcurrentRepository(BaseRedisService, BaseConcurrentRepository):
+    _locked: str = False
+
+    def lock(self, session_id: int, lead_id: int = "x"):
+        if self._locked and self._locked != f"{session_id}-{lead_id}":
+            return False
+
+        self._locked = f"{session_id}-{lead_id}"
+        return True
+
+    def unlock(self):
+        self._locked = False
+
+    def wait_for_lock(self, session_id: int, lead_id: int = "x"):
+        c = 0
+
+        while c < 1500:
+            if self.lock(session_id, lead_id):
+                return True
+
+            time.sleep(.01)
+            c += 1
+
+    @classmethod
+    def locked(
+            cls, only_session_id: bool = False,
+            only_one_thread: bool = False,
+            session_id_position: int = 1,
+            lead_id_position: int = 2,
+            session_id_kwarg: str = "session_id",
+            lead_id_kwarg: str = "lead_id"):
+        def wrapper(func):
+            def wrapped(*args, **kwargs):
+                self: DefaulConcurrentRepository = args[0]
+
+                if not issubclass(type(self), cls):
+                    raise TypeError("Not correct decorator place")
+
+                session_id = kwargs.get(
+                    session_id_kwarg, args[session_id_position % len(args)]
+                )
+
+                if not only_session_id:
+                    lead_id = kwargs.get(
+                        lead_id_kwarg, args[lead_id_position % len(args)]
+                    )
+                else:
+                    lead_id = None if not only_one_thread else (
+                        random.randint(1000, 2000)
+                    )
+
+                self.wait_for_lock(session_id=session_id,
+                                   lead_id=lead_id)
+
+                result = func(*args, **kwargs)
+
+                self.unlock()
+
+                return result
+
+            return wrapped
+
+        return wrapper
