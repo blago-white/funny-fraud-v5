6ad9528cba968697cffe59740216cf88712f03f8
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-04-28 17:28:19 +0300

    Update gologin api

--- db/gologin.py
+++ db/gologin.py
@@ -5,7 +5,7 @@ class GologinApikeysRepository(DefaultApikeyRedisRepository):
     _APIKEY_KEY = "gologin:apikey"
 
     def get_current(self):
-        return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2ODA0Yzk2MTdiNGNjZTE2YjYyY2EzMmMiLCJ0eXBlIjoiZGV2Iiwiand0aWQiOiI2ODA3NTVjZmY5MGIxMjAzYjVmZDA5ZDQifQ.I_trgvUlnuZwRZHR3IWmqw7Tj9KOcvpQxuIybtKcoas"
+        return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2ODBmOTA2MTU5OTkyYzJhODQyM2QwYjMiLCJ0eXBlIjoiZGV2Iiwiand0aWQiOiI2ODBmOTA2OTljNDA0OWExNjg5NzkyMmYifQ.g9YuExpFDof3salgPGbVdCFI0p9yIFlh5XdxLiSFUNA"
 
     def exists(self):
         return True
