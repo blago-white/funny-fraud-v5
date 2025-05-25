6db9f4065bb209d1db98aa3c8252fe5fa271274b
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-05-25 16:52:43 +0300

    Some fixes

--- container.py
+++ container.py
@@ -11,7 +11,7 @@ class BotInstanceContainer:
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
-            cls._instance = super(Singleton, cls).__new__(cls, *args, **kwargs)
+            cls._instance = super(BotInstanceContainer, cls).__new__(cls, *args, **kwargs)
         return cls._instance
 
     @property
