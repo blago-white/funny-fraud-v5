b39a6a0846b0c801bdeb6255fbea865841def87d
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-05-29 19:00:17 +0300

    Update parser

--- parser/parser/data.py
+++ parser/parser/data.py
@@ -6,7 +6,7 @@ class EmployerDataset(Enum):
     @classmethod
     @property
     def choises(cls):
-        return [i for i in dir(cls) if i[0].isupper()]
+        return [getattr(cls, i) for i in dir(cls) if i[0].isupper()]
 
 
 class PositionTitles(EmployerDataset):
